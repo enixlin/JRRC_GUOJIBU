3b5f1a110a108d60e6998709a0815b42580d7d7b
linzhenhuan <linzhenhuan@DESKTOP-BCMBHHF>
2017-03-05 16:29:42 +0800

    add UpdateService

---
+++ src/enixlin/jrrc/service/UpdateService.java
@@ -0,0 +1,13 @@
+package enixlin.jrrc.service;
+
+/**
+* 
+* @ClassName: UpdateService
+* @Description:数据库更新接口
+* @author linzhenhuan
+* @date 2017年3月5日
+*
+*/
+public interface UpdateService {
+
+}
